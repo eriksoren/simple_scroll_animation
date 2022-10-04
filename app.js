2c9894358ce4398c0e366595f894c452f91ba0a4
eriksoren <eriksrln@gmail.com>
2022-10-04 12:46:26 +0200

    finito

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
 
-const observer = new IntersectionObserver((entries) => {
+const observer_x = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.log(entry)
+        //console.log(entry)
         if(entry.isIntersecting) {
-            entry.target.classList.add("show")
+            entry.target.classList.add("show_vertical"); 
         } else {
-            entry.target.classList.remove("show")
+            entry.target.classList.remove("show_vertical");
         }
     }); 
 }); 
 
-const hiddenElements = document.querySelectorAll('.hidden'); 
-hiddenElements.forEach((el) => observer.observe(el)); 
+const observer_y = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        //console.log(entry)
+        if(entry.isIntersecting) {
+            entry.target.classList.add("show_horizontel"); 
+        } else {
+            entry.target.classList.remove("show_horizontel"); 
+        }
+    }); 
+}); 
+
+// Animation from vertical position
+const hidden_left_Elements = document.querySelectorAll('.hidden_left'); 
+const hidden_right_Elements = document.querySelectorAll('.hidden_right');
+
+hidden_right_Elements.forEach((el) => observer_x.observe(el)); 
+hidden_left_Elements.forEach((el) => observer_x.observe(el)); 
+
+// Animation from the horizontel position
+const hidden_bottom_Elements = document.querySelectorAll('.hidden_bottom');
+const hidden_top_Elements = document.querySelectorAll('.hidden_top'); 
+
+hidden_bottom_Elements.forEach((el) => observer_y.observe(el)); 
+hidden_top_Elements.forEach((el) => observer_y.observe(el)); 
